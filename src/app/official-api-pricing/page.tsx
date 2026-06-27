4ed9d9773b691bce18ec7f148370db4e241b8428
usari14 <udogar583@gmail.com>
2026-06-28 02:28:23 +0500

    build fix

--- src/app/official-api-pricing/page.tsx
+++ src/app/official-api-pricing/page.tsx
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-"use client";
-
-import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import OfficialApiPricing from "@/components/OfficialApiPricing";
 import OfficialApiCostCalculator from "@/components/OfficialApiCostCalculator";
-import OfficialApiPricing from "@/components/OfficialApiPricing";
-import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
-import LoadingBar from "react-top-loading-bar";
-=======
-import Navbar from "@/components/Navbar";
-import OfficialApiPricing from "@/components/OfficialApiPricing";
 import Footer from "@/components/Footer";
 import PageLoadingBar from "@/components/PageLoadingBar";
 import { Metadata } from "next";
@@ -28,7 +18,6 @@ export const metadata: Metadata = {
     description: "Check our official WhatsApp Cloud API pricing. Connect your own Meta business account and send automated notification messages directly to customers.",
   }
 };
->>>>>>> 908b3178e43f26ee3c1217d681a66b3b63ee95bf
 
 export default function OfficialApiPricingPage() {
   return (
